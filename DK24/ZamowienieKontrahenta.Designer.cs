b20d80f3a293ae6a75d3e89588c9540a3a736c51
Szufrajda <s25223@pjwstk.edu.pl>
2024-11-06 13:46:35 +0100

    Dodanie zamowien dla Kontrahenta i Klienta

---
+++ DK24/ZamowienieKontrahenta.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace DK24
+{
+    partial class ZamowienieKontrahenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZamowienieKontrahenta));
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            dataGridView3 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            dataGridView1 = new DataGridView();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(594, 52);
+            label9.Name = "label9";
+            label9.Size = new Size(80, 15);
+            label9.TabIndex = 23;
+            label9.Text = "ZAMÓWIENIE";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(43, 347);
+            label8.Name = "label8";
+            label8.Size = new Size(124, 15);
+            label8.TabIndex = 22;
+            label8.Text = "WYBIERZ PARAMETRY";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(43, 81);
+            label7.Name = "label7";
+            label7.Size = new Size(110, 15);
+            label7.TabIndex = 21;
+            label7.Text = "WYBIERZ PRODUKT";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(738, 293);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 15);
+            label6.TabIndex = 20;
+            label6.Text = "10.00 zł";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(629, 293);
+            label5.Name = "label5";
+            label5.Size = new Size(98, 15);
+            label5.TabIndex = 19;
+            label5.Text = "ŁĄCZNA KWOTA:";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(821, 280);
+            button3.Name = "button3";
+            button3.Size = new Size(130, 41);
+            button3.TabIndex = 18;
+            button3.Text = "Podsumuj zamówienie";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(43, 255);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 17;
+            button2.Text = "Wybierz";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(43, 539);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 42);
+            button1.TabIndex = 16;
+            button1.Text = "Dodaj do zamówienia";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(620, 365);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(350, 231);
+            groupBox1.TabIndex = 15;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Kontrahent";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(104, 121);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(45, 23);
+            comboBox1.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(104, 161);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(174, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(155, 121);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(123, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(104, 80);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(174, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(104, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(174, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(48, 164);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Nr telefonu:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(58, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 1;
+            label2.Text = "NIP:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nazwa:";
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(594, 70);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(394, 201);
+            dataGridView3.TabIndex = 14;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(43, 99);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(478, 150);
+            dataGridView2.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(43, 365);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(478, 164);
+            dataGridView1.TabIndex = 12;
+            // 
+            // ZamowienieKontrahenta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1030, 648);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView3);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ZamowienieKontrahenta";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ZamowienieKontrahenta";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox1;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DataGridView dataGridView3;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ DK24/ZamowinieKlient.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace DK24
+{
+    partial class ZamowinieKlient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZamowinieKlient));
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            dataGridView3 = new DataGridView();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(41, 373);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(478, 164);
+            dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(41, 107);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(478, 150);
+            dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(592, 78);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(394, 201);
+            dataGridView3.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(618, 373);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(350, 231);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Klient";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Imie:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nazwisko:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Nr telefonu:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(48, 164);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(104, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(174, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(104, 80);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(174, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(155, 121);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(123, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(104, 161);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(174, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(104, 121);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(45, 23);
+            comboBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 547);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 42);
+            button1.TabIndex = 4;
+            button1.Text = "Dodaj do zamówienia";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(41, 263);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Wybierz";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(819, 288);
+            button3.Name = "button3";
+            button3.Size = new Size(130, 41);
+            button3.TabIndex = 6;
+            button3.Text = "Podsumuj zamówienie";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(627, 301);
+            label5.Name = "label5";
+            label5.Size = new Size(98, 15);
+            label5.TabIndex = 7;
+            label5.Text = "ŁĄCZNA KWOTA:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(736, 301);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 15);
+            label6.TabIndex = 8;
+            label6.Text = "10.00 zł";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(41, 89);
+            label7.Name = "label7";
+            label7.Size = new Size(110, 15);
+            label7.TabIndex = 9;
+            label7.Text = "WYBIERZ PRODUKT";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(41, 355);
+            label8.Name = "label8";
+            label8.Size = new Size(124, 15);
+            label8.TabIndex = 10;
+            label8.Text = "WYBIERZ PARAMETRY";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(592, 60);
+            label9.Name = "label9";
+            label9.Size = new Size(80, 15);
+            label9.TabIndex = 11;
+            label9.Text = "ZAMÓWIENIE";
+            // 
+            // ZamowinieKlient
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1030, 648);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView3);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ZamowinieKlient";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ZamowinieKlient";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView3;
+        private GroupBox groupBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+    }
+}
